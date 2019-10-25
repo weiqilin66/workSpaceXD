f092c3c42e2cf4d26b2b2d0f1c150428f32ed8d2
weiqilin66 <linweiqi45vip>
2019-10-25 16:25:06 +0800

    null

--- workspace/ideaworkspace/NIO/src/main/java/com/readwrite/main/APP.java
+++ workspace/ideaworkspace/NIO/src/main/java/com/readwrite/main/APP.java
@@ -2,6 +2,7 @@ package com.readwrite.main;
 
 import com.readwrite.entity.TableQ;
 import com.readwrite.service.ReadExcelServiceImpl;
+import com.readwrite.service.WriteJSON;
 import com.readwrite.service.WriteVue;
 import com.readwrite.utils.CnOrUs;
 import com.readwrite.utils.ReadReplaceInsert;
@@ -12,8 +13,9 @@ import java.io.*;
 import java.util.Map;
 
 public class APP {
-    public static void main(String[] args)  {
-        WriteVue.mvvm("e:/testArea/exmple.txt","e:/testArea/物理模型.xls","e:/testArea/exmple.txt",6);
+    public static void main(String[] args) throws Exception {
+        //WriteVue.mvvm(e:/testArea/exmple.txt"e:/testArea/物理模型.xls","e:/testArea/exmple.txt",6);
+        WriteJSON.writeJ("e:/testArea/物理模型.xls","e:/testArea/json.txt",6,"xxinfo");
     }
 
 
--- workspace/ideaworkspace/NIO/src/main/java/com/readwrite/service/WriteJSON.java
+++ workspace/ideaworkspace/NIO/src/main/java/com/readwrite/service/WriteJSON.java
@@ -1,5 +1,8 @@
 package com.readwrite.service;
 
+import com.readwrite.entity.TableQ;
+import com.readwrite.utils.ExcelToJson;
+import com.readwrite.utils.ToCamelFormat;
 import sun.rmi.transport.ObjectTable;
 
 import java.io.File;
@@ -8,25 +11,22 @@ import java.io.RandomAccessFile;
 import java.util.Map;
 
 public class WriteJSON {
-
-    public String writeJ(Map<Integer, Object> map,String targetPath)throws IOException {
-        String str ="";
-
-
-
-
-
+    /**
+     *
+     * @param excelPath
+     * @param targetPath 目标文件写入
+     * @param headSize excel的字段数 默认6
+     * @param headStr json的主key
+     * @throws Exception
+     */
+    public static void writeJ(String excelPath,String targetPath,int headSize,String headStr) throws Exception {
+        String JsonStr = ExcelToJson.toJ(excelPath,headSize,headStr);
 
         File file = new File(targetPath);
         RandomAccessFile raf = new RandomAccessFile(file,"rw");
         long index = raf.length();
         raf.seek(index);
-        raf.write(str.getBytes());
-        String JsonStr = null;
-
-
-
-
-        return JsonStr;
+        raf.write(
+                (",\n"+JsonStr).getBytes());
     }
 }
