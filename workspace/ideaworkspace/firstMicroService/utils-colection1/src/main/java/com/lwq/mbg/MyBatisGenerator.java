17743acb079500948ee67e47bf52086a3435f864
weiqilin66 <linweiqi45vip>
2019-10-30 17:57:15 +0800

    1

---
+++ workspace/ideaworkspace/firstMicroService/utils-colection1/src/main/java/com/lwq/mbg/MyBatisGenerator.java
@@ -0,0 +1,22 @@
+package com.lwq.mbg;
+
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBatisGenerator {
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("src/mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        org.mybatis.generator.api.MyBatisGenerator myBatisGenerator = new org.mybatis.generator.api.MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
