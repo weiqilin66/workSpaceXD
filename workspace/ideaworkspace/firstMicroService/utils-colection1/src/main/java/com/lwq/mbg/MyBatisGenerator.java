b426ed6553d82f746bc2e2f4354699c80f7714e9
weiqilin66 <linweiqi45vip>
2019-11-11 10:28:29 +0800

    1

--- workspace/ideaworkspace/firstMicroService/utils-colection1/src/main/java/com/lwq/mbg/MyBatisGenerator.java
+++ workspace/ideaworkspace/firstMicroService/utils-colection1/src/main/java/com/lwq/mbg/MyBatisGenerator.java
@@ -12,7 +12,7 @@ public class MyBatisGenerator {
     public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-        File configFile = new File("src/mbg.xml");
+        File configFile = new File("/generator/mbg.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
