40c805eca65d0ea2f5642b6e5d8741a93ec62e85
weiqilin66 <linweiqi45vip>
2019-12-12 16:52:05 +0800

    ;

---
+++ workspace/ideaworkspace/ccbMicroService1910/lwq-common/src/test/java/com/lwq/lwqcommon/LwqCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwq.lwqcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwqCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
