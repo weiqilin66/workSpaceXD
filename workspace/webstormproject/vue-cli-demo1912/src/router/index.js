ac30041c2bcd97ac1b5cc88ab3eb278abce527b8
weiqilin66 <linweiqi45vip>
2019-12-23 19:59:08 +0800

    l

--- workspace/webstormproject/vue-cli-demo1912/src/router/index.js
+++ workspace/webstormproject/vue-cli-demo1912/src/router/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-// 利用 vue-router 的 addRoutes 方法可以动态添加路由
+/*
+初始路由只配置login 404
+利用 vue-router 的 addRoutes 方法可以动态添加路由
+*/
 export default new Router({
   // mode: 'history',
   routes: [
@@ -16,34 +19,15 @@ export default new Router({
       redirect: '/login'
     },
     {
+      path: '*',
+      name: '404',
+      component: () => import('@/views/404')
+    }
+    /*{
       path: '/homePage',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/Dashboard')
       // redirect:'子组件名' redirect到某个children组件可以默认显示哪个子组件
-    },
-    {
-      path: '/404',
-      component: () => import('@/views/404')
-    }
+    }*/
   ]
 })
-/*// 从服务器获取路由,进行拼接
-export const makeRoute = function (items) {
-  let routes = []
-  for (let i = 0; i < items.length; i++) {
-    console.log(items[i])
-    items[i] = formatRoute(items[i])
-    if (items[i].children) {
-      // 递归处理子路由的component
-      items[i].children = makeRoute(items[i].children)
-    }
-  }
-  return items
-}
-
-// 格式化路由,使component挂载到路由上,生成addRoutes可用的格式
-const formatRoute = (item) => {
-  let route = item
-  route.component = eval(route.component)
-  return route
-}*/
