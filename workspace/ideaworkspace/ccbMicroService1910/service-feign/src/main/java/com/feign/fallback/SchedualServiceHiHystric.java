f75d8c6e920696e9dd22e79281753c846eee5389
weiqilin66 <weiqilin66@gmail.com>
2020-01-02 16:16:17 +0800

    ''

---
+++ workspace/ideaworkspace/ccbMicroService1910/service-feign/src/main/java/com/feign/fallback/SchedualServiceHiHystric.java
@@ -0,0 +1,17 @@
+package com.feign.fallback;
+
+import com.feign.service.FeignService;
+import feign.Feign;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description: 熔断方法需要实现 熔断的接口
+ * @author: LinWeiQi
+ */
+@Component
+public class SchedualServiceHiHystric implements FeignService {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
---
+++ workspace/ideaworkspace/ccbMicroService1910/service-feign/src/main/java/com/feign/service/FeignService.java
@@ -0,0 +1,16 @@
+package com.feign.service;
+
+import com.feign.fallback.SchedualServiceHiHystric;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description: 描述
+ * @author: LinWeiQi
+ */
+@FeignClient(value="TEST-CONNECT" ,fallback = SchedualServiceHiHystric.class) // 通过@ FeignClient（“服务名”），来指定调用哪个服务
+public interface FeignService {
+    @GetMapping("/hi")
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
