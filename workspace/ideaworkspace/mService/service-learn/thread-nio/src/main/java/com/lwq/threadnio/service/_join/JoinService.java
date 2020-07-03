a5b3c82c709ac8c755280eb14ce0bd1e794c8c7f
weiqilin66 <weiqilin66@gmail.com>
2020-07-03 15:35:17 +0800

    1

--- workspace/ideaworkspace/mService/service-learn/thread-nio/src/main/java/com/lwq/threadnio/service/_join/JoinService.java
+++ workspace/ideaworkspace/mService/service-learn/thread-nio/src/main/java/com/lwq/threadnio/service/_join/JoinService.java
@@ -1,9 +1,10 @@
-package com.lwq.threadnio.service.join;
+package com.lwq.threadnio.service._join;
 
 /**
  * @Description: 主线程需要等待子线程执行完成之后再结束
+join(long millis)、join(long millis, int nanos)两个具有超时特性的方法。
+这两个超时方法表示，如果线程thread在指定的超时时间没有终止，那么将会从该超时方法中返回。
  * Thread.sleep(2000)不会释放锁，threadTest.join(2000)会释放锁 。
- * @author: LinWeiQi
  */
 public class JoinService {
     public static void main(String[] args) throws InterruptedException {
